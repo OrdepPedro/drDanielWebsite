5738418ccd97c35332db82505ca0cc24fe8e49be
Pedro Fontoura <pedroafontoura@outlook.com>
2020-07-29 09:18:23 -0300

    Reseting the page title, fixed some balls position, added smooth scroll with js

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,5 +1,6 @@
 // Sticky navbar
 const navbar = document.querySelector(".navbar");
+const links = document.querySelectorAll(".navbar ul a");
 
 window.onload = navbar.classList.remove("top");
 
@@ -10,3 +11,19 @@ window.onscroll = function () {
     navbar.classList.remove("top");
   }
 };
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop - 100;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth",
+  });
+
+  e.preventDefault();
+}
